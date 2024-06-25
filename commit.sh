141dbf6e4dd00bb2307ab311bc4c5ca5fd6e5e85
bruno-psouza <brunops83@gmail.com>
2024-06-25 11:57:06 -0300

    Adicionar arquivos para o GitHub

---
+++ commit.sh
@@ -0,0 +1,15 @@
+# Verificar o status do repositório
+git status
+
+# Adicionar todos os arquivos ao índice do Git
+git add .
+
+# Fazer o commit das mudanças
+git commit -m "Adicionar arquivos para o GitHub"
+
+# Verificar o branch atual
+git branch
+
+# Enviar as mudanças para o repositório remoto (substitua 'master' pelo seu branch atual)
+git push -u origin master
+
